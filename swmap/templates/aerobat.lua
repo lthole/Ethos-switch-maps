df749db15cd1077d0e8ae1d6cd7bd602035b5e6e
Lothar Thole <lothar.thole@gmail.com>
2025-07-10 15:00:06 +0930

    Update swmap files

---
+++ swmap/templates/aerobat.lua
@@ -0,0 +1,67 @@
+return {
+   DisplayAll = true,
+
+   DisplaySwitchNames = false,
+
+   SAtext = "Stab",
+
+   SBtext = "",
+
+   SCtext = "Rates",
+
+   SDtext = "",
+
+   SEtext = "",
+
+   SFtext = "",
+
+   SGtext = "Thr arm",
+
+   SHtext = "Thr cancel + SGdn",
+
+   SItext = "",
+
+   SJtext = "",
+
+   T1text = "",
+
+   T2text = "Motor comp",
+
+   T3text = "",
+
+   T4text = "",
+
+   LStext = "",
+
+   RStext = "",
+
+   S1text = "Stab gain",
+
+   S2text = "SL gain",
+
+   S3text = "Volume",
+
+   FS1text = "",
+
+   FS2text = "",
+
+   FS3text = "",
+
+   FS4text = "",
+
+   FS5text = "",
+
+   FS6text = "",
+
+   SKtext = "",
+
+   SLtext = "",
+
+   T5text = "",
+
+   T6text = "Instant trim",
+
+   TextColor = 251660287,
+
+   ControlsColor = 251723264
+}
---
+++ swmap/templates/glider.lua
@@ -0,0 +1,67 @@
+return {
+   DisplayAll = true,
+
+   DisplaySwitchNames = false,
+
+   SAtext = "Stab",
+
+   SBtext = "Call telem",
+
+   SCtext = "",
+
+   SDtext = "",
+
+   SEtext = "FMode",
+
+   SFtext = "",
+
+   SGtext = "Thr arm",
+
+   SHtext = "Thr cancel + SGdn",
+
+   SItext = "",
+
+   SJtext = "",
+
+   T1text = "",
+
+   T2text = "Flap comp/Motor comp",
+
+   T3text = "",
+
+   T4text = "Ail diff",
+
+   LStext = "Thermal camber",
+
+   RStext = "Throttle",
+
+   S1text = "Stab gain",
+
+   S2text = "SL gain",
+
+   S3text = "Volume",
+
+   FS1text = "",
+
+   FS2text = "",
+
+   FS3text = "",
+
+   FS4text = "",
+
+   FS5text = "",
+
+   FS6text = "",
+
+   SKtext = "",
+
+   SLtext = "",
+
+   T5text = "",
+
+   T6text = "",
+
+   TextColor = 251660287,
+
+   ControlsColor = 251723264
+}
