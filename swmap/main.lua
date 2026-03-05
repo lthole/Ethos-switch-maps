b24598d9519ee86fb30c476728fff46e36fed0ff
Eric Delord <04-ete.elit@icloud.com>
2026-03-05 23:32:36 +0100

    chore: changed log color to cyan for readability

--- swmap/main.lua
+++ swmap/main.lua
@@ -42,10 +42,11 @@ local ANSI_BLACK = "\27[1;30m"
 local ANSI_RED = "\27[1;31m"
 local ANSI_GREEN = "\27[1;32m"
 local ANSI_YELLOW = "\27[1;33m"
+local ANSI_CYAN = "\27[0;36m"
 local function log(text, ansiColor)
-    if not ansiColor then ansiColor = ANSI_BLACK end
+    if not ansiColor then ansiColor = ANSI_CYAN end -- black is unreadable on ethos.studio1247.com
     local ANSI_RESET = "\27[0m"
-    print(ansiColor..text..ANSI_RESET)
+    print(ansiColor..tostring(text)..ANSI_RESET)
 end
 if debug_mode then log("SWMAP Debug MODE ON") end
 
